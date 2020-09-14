d5734f36f20faba7f9c60815ad2634395c311c0c
Zhaoyao <1307526550@qq.com>
2020-09-14 20:41:50 +0800

    commit

---
+++ python/094_inorderTraversal.py
@@ -0,0 +1,47 @@
+"""
+94. 二叉树的中序遍历
+给定一个二叉树，返回它的中序 遍历。
+
+示例:
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from typing import List
+from python.binaryTree import TreeNode
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        """
+
+        :param root:
+        :return:
+        """
+        if root is None:
+            return []
+
+        seq = []
+        stack = []
+        while root is not None or len(stack) != 0:
+            if root is not None:
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                seq.append(root.val)  # left child pop first, then root
+                root = root.right
+        return seq
--- python/binaryTree.py
+++ python/binaryTree.py
@@ -40,7 +40,133 @@ class TreeNode:
                 A.append(current_root.right)
         return result
 
+    # Non-recursive preorder traversal
+    def preorderTraversal(self, root):
+        """
+
+        :param root:
+        :return:
+        """
+        if root is None:
+            return []
+
+        seq = []
+        stack = []
+        while root is not None or len(stack) != 0:
+            if root is not None:
+                seq.append(root.val)
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                root = root.right
+        return seq
+
+    # Non-recursive inorder traversal
+    def inorderTraversal(self, root):
+        """
+
+        :param root:
+        :return:
+        """
+        if root is None:
+            return []
+
+        seq = []
+        stack = []
+        while root is not None or len(stack) != 0:
+            if root is not None:
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                seq.append(root.val)  # left child pop first, then root
+                root = root.right
+        return seq
+
+    # Non-recursive postorder traversal
+    # 把先序顺序中的 ‘根左右’ 转换为 ‘根右左’，然后反过来就变成了‘左右根’。
+    def postorderTraversal(self, root):
+        """
+
+        :param root:
+        :return:
+        """
+        if root is None:
+            return []
+
+        seq = []
+        stack = []
+        output = []
+        while root or len(stack) != 0:
+            if root:
+                seq.append(root.val)
+                stack.append(root)
+                root = root.right
+            else:
+                root = stack.pop()
+                root = root.left
+
+        while seq:
+            output.append(seq.pop())
+
+        return output
+
+    def layerorderTraversal(self, root):
+        """
+
+        :param root:
+        :return:
+        """
+        if root is None:
+            return []
+
+        seq = []
+        queue = []
+        queue.append(root)
+        while queue:
+            root = queue.pop(0)
+            seq.append(root.val)
+            if root.left:
+                queue.append(root.left)
+            if root.right:
+                queue.append(root.right)
+        return seq
+
 # llist = [1, 2, 3, 4, 5]
 # A = TreeNode(1)
 # A.createTreeWithList(None, llist, 0)
 # print(A.function(A))
+
+a = TreeNode(1)
+b = TreeNode(2)
+c = TreeNode(3)
+d = TreeNode(4)
+e = TreeNode(5)
+f = TreeNode(6)
+g = TreeNode(7)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.left = f
+c.right = g
+
+"""
+      1
+    /   \
+   2     3
+  / \   / \
+ 4   5 6   7 
+ preorder   : 1245367
+ inorder    : 4251637
+ postorder  : 4526731
+ layerorder : 1234567
+"""
+
+
+print("preorder Traversal: ", a.preorderTraversal(a))
+print("inorder Traversal: ", a.inorderTraversal(a))
+print("postorder Traversal: ", a.postorderTraversal(a))
+print("layerorder Traversal: ", a.layerorderTraversal(a))
