53c4f1ec4871dc239b687a9143e2564e8bf8db5c
Zhaoyao <1307526550@qq.com>
2020-11-14 10:43:24 +0800

    添加方法buildList, printList, reverseList

--- python/singlyLinkedList.py
+++ python/singlyLinkedList.py
@@ -1,5 +1,55 @@
 # Definition for singly-linked list.
+from typing import List
+
+
 class ListNode:
-    def __init__(self, x):
+    def __init__(self, x=0):
         self.val = x
         self.next = None
+
+    # Build Singly Linked List with int List
+    def buildList(self, nums: List):
+        """
+
+        :param nums: input int list to build Singly Linked List
+        :return: head of the built Singly Linked List
+        """
+
+        head = ListNode(0)
+        head_point = head
+        for i in range(len(nums)):
+            node = ListNode(nums[i])
+            head.next = node
+            head = head.next
+        return head_point.next
+
+    # Format printout linked list
+    def printList(self, head):
+        """
+
+        :param head: head node of the Linked List
+        :return: No return
+        """
+        res = ""
+        while head is not None:
+            if head.next is not None:
+                res = res + str(head.val) + " -> "
+            else:
+                res = res + str(head.val)
+            head = head.next
+        print(res)
+
+    # Reverse Linked List
+    def reverseList(self, head):
+        """
+
+        :param head: head node of the Linked List
+        :return: new head
+        """
+        reverse_head = None
+        while head:
+            next = head.next
+            head.next = reverse_head
+            reverse_head = head
+            head = next
+        return reverse_head
