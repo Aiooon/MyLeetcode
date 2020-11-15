fbcf85a1c892fff78bf5b069890a52123e6a9102
Zhaoyao <1307526550@qq.com>
2020-11-15 11:18:49 +0800

    21. 合并两个有序链表

--- python/021_mergeTwoLists.py
+++ python/021_mergeTwoLists.py
@@ -19,21 +19,17 @@ from python.singlyLinkedList import ListNode
 
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        mergehead = ListNode()
+        mergehead = ListNode(-1)
         head = mergehead
         while l1 and l2:
             if l1.val < l2.val:
                 mergehead.next = l1
-                mergehead = mergehead.next
                 l1 = l1.next
             else:
                 mergehead.next = l2
-                mergehead = mergehead.next
                 l2 = l2.next
-        if l1:
-            mergehead.next = l1
-        if l2:
-            mergehead.next = l2
+            mergehead = mergehead.next
+        mergehead.next = l1 if l1 else l2
         return head.next
 
 
