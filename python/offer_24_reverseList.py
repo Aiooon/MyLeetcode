ff6fe069086515eea0786e76906d9e5ab542d26b
zy <1307526550@qq.com>
2020-12-22 21:08:54 +0800

    剑指 Offer 24. 反转链表

--- python/offer_24_reverseList.py
+++ python/offer_24_reverseList.py
@@ -13,3 +13,38 @@
 
 date : 12-22-2020
 """
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        nums = []
+        cur = self
+        while cur:
+            nums.append(cur.val)
+            cur = cur.next
+        return " -> ".join(str(num) for num in nums)
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        newHead = None
+        cur = head
+        while cur:
+            newNode = ListNode(cur.val)
+            newNode.next = newHead
+            newHead = newNode
+            cur = cur.next
+        return newHead
+
+
+if __name__ == '__main__':
+    head = ListNode(1)
+    cur = head
+    for i in range(2, 6):
+        newNode = ListNode(i)
+        cur.next = newNode
+        cur = cur.next
+    print(head)
+    print(Solution().reverseList(head))
