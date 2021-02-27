99a6f0ce057abcac763b85750a52637eff19ddac
zy <1307526550@qq.com>
2021-02-27 21:20:50 +0800

    solved

---
+++ python/offer_59.1_滑动窗口的最大值.py
@@ -0,0 +1,63 @@
+"""
+剑指 Offer 59 - I. 滑动窗口的最大值
+给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
+
+示例:
+
+输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+输出: [3,3,5,5,6,7]
+解释: 
+
+滑动窗口的位置                最大值
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+
+提示：你可以假设 k 总是有效的，在输入数组不为空的情况下，1 ≤ k ≤ 输入数组的大小。
+
+注意：本题与主站 239 题相同：https://leetcode-cn.com/problems/sliding-window-maximum/
+
+date: 2021年2月27日
+"""
+from typing import List
+import collections
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # overtime
+        """ if not nums or k == 0:
+            return []
+        l, r, res = 0, k, []
+        while r <= len(nums):
+            res.append(max(nums[l:r]))
+            l += 1
+            r += 1
+        return res """
+
+        # todo 双端队列
+        if not nums or k == 0: return []
+        deque = collections.deque()
+        for i in range(k): # 未形成窗口
+            while deque and deque[-1] < nums[i]:
+                deque.pop()
+            deque.append(nums[i])
+        res = [deque[0]]
+        for i in range(k, len(nums)): # 形成窗口后
+            if deque[0] == nums[i - k]:
+                deque.popleft()
+            while deque and deque[-1] < nums[i]:
+                deque.pop()
+            deque.append(nums[i])
+            res.append(deque[0])
+        return res
+
+
+
+nums = [1, 3, -1, -3, 5, 3, 6, 7]
+k = 3
+print(Solution().maxSlidingWindow(nums, k))
