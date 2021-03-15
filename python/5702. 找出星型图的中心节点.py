0c56581368037a26eb94e57910b48e665b40c886
ZYao <1307526550@qq.com>
2021-03-15 10:53:22 +0800

    solved

---
+++ python/5702. 找出星型图的中心节点.py
@@ -0,0 +1,54 @@
+"""
+有一个无向的 星型 图，由 n 个编号从 1 到 n 的节点组成。
+星型图有一个 中心 节点，并且恰有 n - 1 条边将中心节点与其他每个节点连接起来。
+
+给你一个二维整数数组 edges ，其中 edges[i] = [ui, vi] 表示在节点 ui 和 vi 之间存在一条边。
+请你找出并返回 edges 所表示星型图的中心节点。
+
+示例 1：
+输入：edges = [[1,2],[2,3],[4,2]]
+输出：2
+解释：如上图所示，节点 2 与其他每个节点都相连，所以节点 2 是中心节点。
+
+示例 2：
+输入：edges = [[1,2],[5,1],[1,3],[1,4]]
+输出：1
+
+提示：
+3 <= n <= 10^5
+edges.length == n - 1
+edges[i].length == 2
+1 <= ui, vi <= n
+ui != vi
+题目数据给出的 edges 表示一个有效的星型图
+
+date: 2021年3月14日
+"""
+
+from typing import List
+
+
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        # 59 / 60 个通过测试用例
+
+        dic = {}
+        l = len(edges)
+        for i in range(l):
+            v1, v2 = edges[i][0], edges[i][1]
+            if v1 in dic:
+                dic[v1] += 1
+                if dic[v1] == l:
+                    return v1
+            else: 
+                dic[v1] = 1
+                
+            if v2 in dic:
+                dic[v2] += 1
+                if dic[v2] == l:
+                    return v2
+            else: 
+                dic[v2] = 1
+
+
+print(Solution().findCenter([[1,3],[2,3]]))
