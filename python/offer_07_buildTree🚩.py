5b7e9095c30e48cef6fc6b750068d4b00119c8b9
ZYao <1307526550@qq.com>
2021-04-06 10:27:06 +0800

    update

--- python/offer_07_buildTree🚩.py
+++ python/offer_07_buildTree🚩.py
@@ -21,8 +21,12 @@ date : 9-22-2020
 
 from typing import List
 
-from python.binaryTree import TreeNode
-
+# from python.binaryTree import TreeNode
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
