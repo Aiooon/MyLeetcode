dfab3855956c8ae68531c6f1a7260dbcbc0ec3a1
ZYao <1307526550@qq.com>
2021-07-07 22:07:16 +0800

    solved

---
+++ javascript/剑指Offer/14- I. 剪绳子.js
@@ -0,0 +1,48 @@
+/* 
+剑指 Offer 14- I. 剪绳子
+给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），
+每段绳子的长度记为 k[0],k[1]...k[m-1] 。
+请问 k[0]*k[1]*...*k[m-1] 可能的最大乘积是多少？
+例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+
+示例 1：
+输入: 2
+输出: 1
+解释: 2 = 1 + 1, 1 × 1 = 1
+
+示例 2:
+输入: 10
+输出: 36
+解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36
+
+提示：
+2 <= n <= 58
+注意：本题与主站 343 题相同：https://leetcode-cn.com/problems/integer-break/
+
+date: 2021年7月7日
+*/
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var cuttingRope = function (n) {
+    if (n === 2) {
+        return 1;
+    }
+    if (n == 3) {
+        return 2;
+    }
+    let c = Math.floor(n / 3);
+    let rest = n % 3;
+    if (rest === 0) {
+        return Math.pow(3, c);
+    } else if (rest === 1) {
+        return Math.pow(3, c - 1) * 4;
+    } else {
+        return Math.pow(3, c) * 2;
+    }
+};
+
+for (let i = 2; i <= 58; i++) {
+    console.log(cuttingRope(i));
+}
